3ed53c00ac644b22d87206d4a719df88dd178728
Ben Zellers <zellersdenver@gmail.com>
2023-06-23 11:43:44 -0400

    Create Product complete

--- Testing/IProductRepository.cs
+++ Testing/IProductRepository.cs
@@ -9,6 +9,9 @@ namespace Testing
         public IEnumerable<Product> GetAllProduct();
         public Product GetProduct(int id);
         public void UpdateProduct(Product product);
-        
+        public void InsertProduct(Product productToInsert);
+        public IEnumerable<Category> GetCategories();
+        public Product AssignCategory();
+
     }
 }
